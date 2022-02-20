85fc125dd7ffb9c7b45789f0fe8264f7f4eeba1c
Roberto Magno Campos <roberto.magno@g.globo>
2022-02-20 18:59:47 -0300

    Versão definitiva SplashScreen

--- app/src/main/java/com/uerj/godev/SplashScreenActivity.kt
+++ app/src/main/java/com/uerj/godev/SplashScreenActivity.kt
@@ -2,11 +2,17 @@ package com.uerj.godev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+        hideStatusBarAndActionBar()
+    }
+
+    private fun hideStatusBarAndActionBar() {
         supportActionBar?.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
     }
 }
