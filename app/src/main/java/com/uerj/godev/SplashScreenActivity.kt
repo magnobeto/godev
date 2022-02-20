397f7b7652202a9686b2d99dc9f11fad601bdfe1
Roberto Magno Campos <roberto.magno@g.globo>
2022-02-20 19:15:00 -0300

    Navegação entre splash e intro ajustada

--- app/src/main/java/com/uerj/godev/SplashScreenActivity.kt
+++ app/src/main/java/com/uerj/godev/SplashScreenActivity.kt
@@ -1,18 +1,32 @@
 package com.uerj.godev
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         hideStatusBarAndActionBar()
+        delayIntro()
     }
 
     private fun hideStatusBarAndActionBar() {
         supportActionBar?.hide()
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+    }
+
+    private fun delayIntro() {
+        val handle = Handler()
+        handle.postDelayed(Runnable { navigateToLoginActivity() }, 6000)
+    }
+
+    private fun navigateToLoginActivity() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
